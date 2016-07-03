fbe1275f128c033356b1d2d527d304948f663ff4
gitdemo-cn <peng.jiang@outlook.com>
2016-07-04 00:38:33 +0800

    update

--- test-build/test-build.sh
+++ test-build/test-build.sh
@@ -7,5 +7,5 @@ cd ${WORKSPACE}/test-build
 sed -i 's/\$\$BUILD_NUMBER\$\$/'${BUILD_NUMBER}'/g' docker-compose.yml
 sed -i 's/\$\$PORT_NUMBER\$\$/'`expr 5000 + ${BUILD_NUMBER}`'/g' docker-compose.yml
 
-rancher-compose --url http://rancherapps.com:8080 --access-key 19228803051A2DB5F0E0 --secret-key 8jHWRbkryBFvgfvzPUajDT2bzGaHFtPJKPcva8Mq -p Demo-Continuous-Integration-Build${BUILD_NUMBER} up -d
+rancher-compose --url http://rancherapps.com:8080 --access-key 8D3DF61DCC31774310CF --secret-key L5qR1bqcuyBxv1M9jVUMPo8tsATBosnzQ9KYUixx -p Demo-Continuous-Integration-Build${BUILD_NUMBER} up -d
 
