a456437cf795ec37dec9334176c0dbbd4294ecc8
gitdemo-cn <peng.jiang@outlook.com>
2016-07-05 09:45:23 +0800

    change cicde model

--- test-build/test-build.sh
+++ test-build/test-build.sh
@@ -7,5 +7,6 @@ cd ${WORKSPACE}/test-build
 sed -i 's/\$\$BUILD_NUMBER\$\$/'${BUILD_NUMBER}'/g' docker-compose.yml
 sed -i 's/\$\$PORT_NUMBER\$\$/'`expr 5000 + ${BUILD_NUMBER}`'/g' docker-compose.yml
 
-rancher-compose --url http://rancherapps.com:8080 --access-key 8D3DF61DCC31774310CF --secret-key L5qR1bqcuyBxv1M9jVUMPo8tsATBosnzQ9KYUixx -p Demo-Continuous-Integration-Build${BUILD_NUMBER} up -d
+#rancher-compose --url http://rancherapps.com:8080 --access-key 8D3DF61DCC31774310CF --secret-key L5qR1bqcuyBxv1M9jVUMPo8tsATBosnzQ9KYUixx -p Demo-Continuous-Integration-Build${BUILD_NUMBER} up -d
 
+rancher-compose --url http://rancherapps.com:8080 --access-key 8D3DF61DCC31774310CF --secret-key L5qR1bqcuyBxv1M9jVUMPo8tsATBosnzQ9KYUixx up -d --upgrade pyapp
