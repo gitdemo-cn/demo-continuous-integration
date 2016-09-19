ad2fb1813a55ecdc919312168436cec10e17d977
gitdemo-cn <peng.jiang@outlook.com>
2016-09-19 10:14:26 +0800

    update

--- test-build/test-build.sh
+++ test-build/test-build.sh
@@ -4,9 +4,9 @@ docker build -t rancherapps.com:5000/python-redis-demo:b${BUILD_NUMBER} .
 docker push rancherapps.com:5000/python-redis-demo:b${BUILD_NUMBER}
 
 cd ${WORKSPACE}/test-build
-sed -i 's/\$\$BUILD_NUMBER\$\$/'${BUILD_NUMBER}'/g' docker-compose.yml
-sed -i 's/\$\$PORT_NUMBER\$\$/'`expr 5000 + ${BUILD_NUMBER}`'/g' docker-compose.yml
+#sed -i 's/\$\$BUILD_NUMBER\$\$/'${BUILD_NUMBER}'/g' docker-compose.yml
+#sed -i 's/\$\$PORT_NUMBER\$\$/'`expr 5000 + ${BUILD_NUMBER}`'/g' docker-compose.yml
 
-#rancher-compose --url http://rancherapps.com:8080 --access-key 8D3DF61DCC31774310CF --secret-key L5qR1bqcuyBxv1M9jVUMPo8tsATBosnzQ9KYUixx -p Demo-Continuous-Integration-Build${BUILD_NUMBER} up -d
+rancher-compose --url http://rancherapps.com:8080 --access-key 8D3DF61DCC31774310CF --secret-key L5qR1bqcuyBxv1M9jVUMPo8tsATBosnzQ9KYUixx -p Demo-CICD up -d
 
-rancher-compose --url http://rancherapps.com:8080 --access-key 8D3DF61DCC31774310CF --secret-key L5qR1bqcuyBxv1M9jVUMPo8tsATBosnzQ9KYUixx -p Demo-CICD up -d --upgrade pyapp
+#rancher-compose --url http://rancherapps.com:8080 --access-key 8D3DF61DCC31774310CF --secret-key L5qR1bqcuyBxv1M9jVUMPo8tsATBosnzQ9KYUixx -p Demo-CICD up -d --upgrade pyapp
