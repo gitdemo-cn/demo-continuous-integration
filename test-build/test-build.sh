564174a5322a2bad6a44a7201e6d9e6c036ca20e
gitdemo-cn <peng.jiang@outlook.com>
2016-12-15 11:31:22 +0800

    update

--- test-build/test-build.sh
+++ test-build/test-build.sh
@@ -6,4 +6,4 @@ docker push rancherapps.com:5000/python-redis-demo:b${BUILD_NUMBER}
 cd ${WORKSPACE}/test-build
 sed -i 's/\$\$BUILD_NUMBER\$\$/'${BUILD_NUMBER}'/g' docker-compose.yml
 
-rancher-compose --url http://rancherapps.com:8080 --access-key 8D3DF61DCC31774310CF --secret-key L5qR1bqcuyBxv1M9jVUMPo8tsATBosnzQ9KYUixx -p Demo-CICD up -d --upgrade pyapp
+rancher-compose --url http://rancherapps.com:8080 --access-key 4DCAF192184584173F1C --secret-key 3NzEi7hia1DtGFsqgZdYEGELb2b7hq1UqxXys3fU -p Demo-CICD up -d --upgrade pyapp
