8c081dd815371440127c27909f9fe1b0504d4a5b
gitdemo-cn <peng.jiang@outlook.com>
2017-03-09 07:22:15 +0800

    update

--- src/app.py
+++ src/app.py
@@ -7,7 +7,7 @@ redis = Redis(host='redis', port=6379)
 @app.route('/')
 def hello():
     redis.incr('hits')
-    return 'Good mornng from Fuzhou! It is very late now, I have seen it %s times.' % redis.get('hits')
+    return 'Good night from Beijing! It is very late now, I have seen it %s times.' % redis.get('hits')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
