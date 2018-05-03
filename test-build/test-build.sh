ae50bf1055780f2e302f3ce04e65248589a3edcc
gitdemo-cn <peng.jiang@outlook.com>
2018-05-03 10:31:25 +0800

    update

--- test-build/test-build.sh
+++ test-build/test-build.sh
@@ -5,5 +5,7 @@ docker push rancherapps.com:5000/python-redis-demo:b${BUILD_NUMBER}
 
 cd ${WORKSPACE}/test-build
 sed -i 's/\$\$BUILD_NUMBER\$\$/'${BUILD_NUMBER}'/g' docker-compose.yml
+sed -i 's/\$\$PORT_NUMBER\$\$/'`expr 5000 + ${BUILD_NUMBER}`'/g' docker-compose.yml
 
-rancher-compose --url http://rancherapps.com:8080 --access-key 4DCAF192184584173F1C --secret-key 3NzEi7hia1DtGFsqgZdYEGELb2b7hq1UqxXys3fU -p Demo-CICD up -d --upgrade pyapp
+#!rancher-compose --url http://rancherapps.com:8080 --access-key 4DCAF192184584173F1C --secret-key 3NzEi7hia1DtGFsqgZdYEGELb2b7hq1UqxXys3fU -p Demo-CICD up -d --upgrade pyapp
+rancher-compose --url http://rancherapps.com:8080 --access-key 4DCAF192184584173F1C --secret-key 3NzEi7hia1DtGFsqgZdYEGELb2b7hq1UqxXys3fU -p python-redis-demo-build${BUILD_NUMBER} up -d
